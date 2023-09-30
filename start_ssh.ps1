fb69a40267bf547917cec9fb2f7eeccdac541400
Brian Keng <bjlkeng@users.noreply.github.com>
2023-09-30 19:54:18 -0400

    Adding additional port mapping for docker container.

--- start_ssh.ps1
+++ start_ssh.ps1
@@ -5,6 +5,7 @@ Write-Host "WSL Machine IP: ""$wsl_ip"""
 netsh int portproxy reset all
 netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=2222 connectport=22 connectaddress=$wsl_ip
 netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=3000 connectport=3000 connectaddress=$wsl_ip
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22222 connectport=22222 connectaddress=$wsl_ip
 
 #for ($i=60000; $i -le 60010; $i++)
 #{
