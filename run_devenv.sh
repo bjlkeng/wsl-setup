871a7923e7a5adec50826b60f10ca36fdf69b210
Brian Keng <bjlkeng@users.noreply.github.com>
2023-12-18 14:22:15 -0500

    Increasing shared memory size so I can increase num workers.

--- run_devenv.sh
+++ run_devenv.sh
@@ -1,11 +1,14 @@
+mkdir -p /home/bjlkeng/.vscode-server-devenv
+
 docker run -d \
     -v /home/bjlkeng/devel:/home/bjlkeng/devel \
     -v /home/bjlkeng/.ssh:/home/bjlkeng/.ssh \
+    -v /home/bjlkeng/.vscode-server-devenv:/home/bjlkeng/.vscode-server \
     --rm \
     --hostname devenv \
     --net=host \
     --gpus all \
-    --shm-size=1g \
+    --shm-size=8g \
     --ulimit memlock=-1 \
     --ulimit stack=67108864 \
     devenv
